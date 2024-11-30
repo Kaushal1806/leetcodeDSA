031613ceb8aa4eeb8c114127acf058b5fc6b1758
Kaushal1806 <105285332+Kaushal1806@users.noreply.github.com>
2024-11-30 19:43:06 +0530

    Time: 1441 ms (60.01%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select v.customer_id,count(v.visit_id) as count_no_trans
-from visits v left join transactions t  
-on v.visit_id = t. visit_id
+from visits v left join transactions t On v.visit_id = t.visit_id
 where t.transaction_id is null 
 group by v.customer_id
--- having t.transaction_id is null
 
+# after group by , dont apply count function on column having null values
+# so count(t.transaction_id) does not work
